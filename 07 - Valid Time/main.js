1d2c0feca166975e829b3cae85bbb4626c9c09b5
Kyle Tucci <87210361+castthecalmingapple@users.noreply.github.com>
2022-12-27 07:11:34 -0500

    Initial Commit Javascriptmas Day 7

---
+++ 07 - Valid Time/main.js
@@ -0,0 +1,53 @@
+function validTime(str) {
+    const [hour, min] = str.split(":")
+    return hour < 25 && min < 60 ? true : false
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('validTime()', () => {
+    it('returns true for valid time', () => {
+        // arrange
+        const str = '13:58';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(true);
+    });
+
+    it('returns false when invalid hours', () => {
+        // arrange
+        const str = '25:51';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 2: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+
+    it('returns false when invalid minutes', () => {
+        // arrange
+        const str = '02:76';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 3: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+});
