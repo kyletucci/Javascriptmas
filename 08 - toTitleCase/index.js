587cb07699d6a8da2720eed344b50b7f091f1776
Kyle Tucci <87210361+castthecalmingapple@users.noreply.github.com>
2023-01-02 18:39:36 -0500

    Initial Commit Javascriptmas Day 8

---
+++ 08 - toTitleCase/index.js
@@ -0,0 +1,11 @@
+function capitalizeWord(word){
+    return word.slice(0,1).toUpperCase() + word.slice(1);
+}
+
+function toTitleCase(str){
+    return str.split(" ").map(capitalizeWord).join(" ")
+}
+
+// Test your functions
+console.log(capitalizeWord("pumpkin"));
+console.log(toTitleCase("pumpkin pranced purposefully across the pond"));
