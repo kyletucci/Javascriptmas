ddf79ca52caea1760fe8ccde3d90af7ece686903
Kyle Tucci <87210361+castthecalmingapple@users.noreply.github.com>
2023-01-10 12:08:29 -0500

    Added comments

--- 06 - Taco Tray/index.js
+++ 06 - Taco Tray/index.js
@@ -1,10 +1,12 @@
 // Review Challenge 4: Taco Tray
 // - Help our chef fill a tray with tacos! 
 
+// Gets a random number of tacos
 function getRandomNumberOfTacos() {
     return new Array(Math.floor(Math.random() * 10) + 1).fill("🌮")
 }
 
+// Adds tacos to taco tray div
 function putTacosOnTray() {
     return getRandomNumberOfTacos().map(function (taco) {
         return `<div class="taco">${taco}</div>`
