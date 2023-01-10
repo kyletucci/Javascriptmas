a4aa26cffe10789ffbb58ebe4ca577c87f65fcd9
Kyle Tucci <87210361+castthecalmingapple@users.noreply.github.com>
2023-01-10 12:06:56 -0500

    Added comments

--- 08 - toTitleCase/index.js
+++ 08 - toTitleCase/index.js
@@ -1,7 +1,9 @@
+// Capitalizes the word
 function capitalizeWord(word){
     return word.slice(0,1).toUpperCase() + word.slice(1);
 }
 
+// Capitalizes all words in sentence
 function toTitleCase(str){
     return str.split(" ").map(capitalizeWord).join(" ")
 }
