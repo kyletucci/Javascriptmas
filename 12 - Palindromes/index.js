95bf0a636c771b9d69304df128eae8259a6d0c06
Kyle Tucci <87210361+castthecalmingapple@users.noreply.github.com>
2023-01-19 21:53:46 -0500

    Initial Commit Javascriptmas Day 12

---
+++ 12 - Palindromes/index.js
@@ -0,0 +1,10 @@
+function isPalindrome(str){
+    return str === [...str].reverse().join('')
+}
+
+// Test your function
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("civic"));
+console.log(isPalindrome("octopus"));
+console.log(isPalindrome("pumpkins"));
+console.log(isPalindrome("madam"));
